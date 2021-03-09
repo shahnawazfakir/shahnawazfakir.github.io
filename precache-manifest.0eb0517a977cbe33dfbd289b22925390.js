bd7f75ce1bc984d31e56f883365586404953021b
shahnawazfakir <shahnawazfakir@gmail.com>
2021-03-09 13:37:00 -0500

    Updates

---
+++ precache-manifest.0eb0517a977cbe33dfbd289b22925390.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e467e84d5e549600671acf0a610f18c6",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "404e60a5f9dceb8c7027",
+    "url": "/test/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "c6ca6c84ee2a02bfc5f5",
+    "url": "/test/static/js/2.75376d99.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/test/static/js/2.75376d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404e60a5f9dceb8c7027",
+    "url": "/test/static/js/main.91d01041.chunk.js"
+  },
+  {
+    "revision": "86c15de76608a72e0067",
+    "url": "/test/static/js/runtime-main.c5060aa3.js"
+  },
+  {
+    "revision": "b4f40561f0ae1c75473e20912a98fc97",
+    "url": "/test/static/media/hashirshoaeb.b4f40561.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/test/static/media/resume.9d3da0e2.pdf"
+  }
+]);
